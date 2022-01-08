a2d1e0f974d29a9cdae952c1e24531488835c5f0
sarjaradhika <sarjaradhika@gmail.com>
2022-01-08 09:24:49 +0530

    changed fee input format

--- FeePayment.java
+++ FeePayment.java
@@ -20,26 +20,28 @@ public class FeePayment {
      String nationality,level;
      
      //parsing the json file
+     //Enter the File path inside the FileReader
 	 Object obj = new JSONParser().parse(new FileReader("C:\\Users\\Radhika\\eclipse-workspace\\Project1\\src\\project1\\FeeStruct.json"));
      
      // typecasting obj to JSONObject
      JSONObject jo = (JSONObject) obj;
      
 	 //for entering the type of fee
-     System.out.println("\nEnter the fee type\n 1.Examfee 2.Appfee\n");
+     System.out.println("\nEnter the fee type\n 1 for Examfee\n 2 for Appfee\n");
      Scanner input=new Scanner(System.in);
-     String FeeType=input.nextLine();
+     int FeeType=input.nextInt();
+     System.out.println(FeeType);
      switch(FeeType){
      //if the feetype is Examination fee then this part is executed
-     case "Examfee": {
+     case 1: {
          
 	     //Exam fee JSONObject is taken into Examinfee
     	 JSONObject Examinfee= (JSONObject) jo.get("Exam Fee");
     	 
 	     //for selecting nationality
 	     System.out.println("Select the nationality\n1.Indian\n2.Foreign \n3.NRI\n4.SAARC\n");
-	     nationality= input.nextLine();
-	     
+	     nationality= input.next();
+	 
 	     String capnat=nationality.toUpperCase();
 	     
          //JSONObject under specified nationality is taken into fee
@@ -48,13 +50,13 @@ public class FeePayment {
 	     //level_course_selecn will take the course type and level and returns the level 
 	     level_course_selcn();
 	     //fetch_fee1 will process the fee object and prints the fee amount
-	     fetch_fee1(fee);
+	     fetch_examinfee(fee);
 	     break;
 	     
 	  
      }
      //if Application fee is selected then this part is executed
-     case "Appfee":
+     case 2:
     	 {
     	 
     		 JSONObject Applnfee= (JSONObject) jo.get("Application Fee");
@@ -72,7 +74,7 @@ public class FeePayment {
 	         JSONObject all_course=((JSONObject) fee.get("ALL_COURSES"));
 	         
 	         //fetch_fee2 will process the fee object and prints the fee amount
-	         fetch_fee2(all_course,level);
+	         fetch_applnfee(all_course,level);
 	         break;
 	    	 }
     	 
@@ -89,21 +91,28 @@ public class FeePayment {
 	 String course,level;
 	 System.out.println("Select a course\n1.Medical\n2.Dental\n3.Ayurveda\n");
      course=input.nextLine();
-     System.out.println("Select a level\n1.UG\n2.PG\n3.Diploma\n");
+     System.out.println("Select a level\n1.UG\n2.UG-Diploma\n3.PG\n");
      level=input.nextLine();
      return level;
  }
  //For Examination fee this function is executed to fetch the amount from JSON
- public static void fetch_fee1(JSONObject nationalty)
+ public static void fetch_examinfee(JSONObject nationalty)
  {
+	 
 		 JSONObject all_course=((JSONObject) nationalty.get("ALL_COURSES")); 
+		 
 	     JSONObject all_level=((JSONObject) all_course.get("ALL_LEVEL"));
+	     
 	     long amount = (long) all_level.get("amount");
 	     
 	     System.out.println("Examination fees is "+amount);
+	    
+	     
+	     
+	 
  }
  //For Application fee this function is executed to fetch the amount from JSON
- public static void fetch_fee2(JSONObject all_course, String level)
+ public static void fetch_applnfee(JSONObject all_course, String level)
  {
 	      
 	     String lvl=level.toUpperCase();
